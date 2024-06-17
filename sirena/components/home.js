a25c2268d292c8ae820c89bb73f41a6ab98ead01
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2024-06-16 23:35:36 -0400

    added more stuff
    added more tabs to nav bar
    got rid of webpack config
    updated expo and other dependencies

--- sirena/components/home.js
+++ sirena/components/home.js
@@ -8,14 +8,81 @@ import {
 import GoalsScreen from "./goals";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { supabase } from "../utils/supabase";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 const Drawer = createDrawerNavigator();
 
 
 function HomeApp({navigation}) {
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <>
+        <View style={{
+                    flex: 0.74, 
+                    alignItems: 'left', 
+                    justifyContent: 'center', 
+                    backgroundColor: '#FF8FAB'}}>
+        
+        <Ionicons style= {{ backgroundColor: '#FF8FAB', paddingLeft: 10}} name="menu" size={40}  onPress={() => navigation.openDrawer()} />
+        </View>
+        <View style={{
+                    flex: 3, 
+                    alignItems: 'center', 
+                    justifyContent: 'center', 
+                    backgroundColor: '#FF8FAB'}}>
+        
             <Text>Home Screen</Text>
+        
+        
             <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
+        </View>
+        </>
+    );
+}
+
+function SkillTree({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Skill Tree</Text>
+        </View>
+    );
+}
+
+function MoodTracker({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Mood Tracker</Text>
+        </View>
+    );
+}
+
+function DBTHelpSheets({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>DBT Help Sheets</Text>
+        </View>
+    );
+}
+
+function DoItTogether({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Do It Together</Text>
+        </View>
+    );
+}
+
+function Art({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Art</Text>
+        </View>
+    );
+}
+
+function YogaMeditation({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Yoga/Meditation</Text>
         </View>
     );
 }
@@ -25,9 +92,26 @@ export default function HomeScreen({navigation}) {
     return (
         <>
             <Drawer.Navigator 
-                          screenOptions={{headerShown: false }}>
+                initialRouteName="Home" 
+                        
+                          screenOptions={{
+                            drawerActiveTintColor: '#FB6F92',
+                                headerShown: false, 
+                                
+                                drawerStyle: {
+                                    backgroundColor: '#FFB3C6',
+                                    width: 200,
+                                    
+
+                                }}}>
                 <Drawer.Screen name="Home" component={HomeApp}/>
-                <Drawer.Screen name="Goals" component={GoalsScreen}/>
+                <Drawer.Screen name="Goal Tracker" component={GoalsScreen}/>
+                <Drawer.Screen name="Skill Tree" component={SkillTree}/>
+                <Drawer.Screen name="Mood Tracker" component={MoodTracker}/>
+                <Drawer.Screen name="DBT Help Sheets" component={DBTHelpSheets}/>
+                <Drawer.Screen name="Do-It Together" component={DoItTogether}/>
+                <Drawer.Screen name="Art" component={Art}/>
+                <Drawer.Screen name="Yoga/Meditation" component={YogaMeditation}/>
             </Drawer.Navigator>
         </>
         
--- sirena/components/loggedinNav.js
+++ sirena/components/loggedinNav.js
@@ -1,6 +1,18 @@
 import HomeScreen from "./home";
 import { Session } from "@supabase/supabase-js";
 
+
+// TODO: When logging in, a new user should have to submit 
+//       -> a Personality Test before they can access the Home Screen.
+//       This is only performed once  for new users 
+//       and the results should be stored in the database.
+//       The Personality Test should be a series of questions 
+//          that the user must complete. Should be short and concise with a bar to show test progress.
+//      -> Then they must customize their character 
+//      -> Finally, their skill tree is assembled based on the results of their Personality Test
+//      -> They can then access the Home Screen
+//     -> Returning users can access the Home Screen immediately
+
 export default function LoginNav({navigation, session}) {
 
     return(
